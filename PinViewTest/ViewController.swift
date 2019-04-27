77bf71d58bf01552cf70234b4f24702e09e00124
Anup Sukumaran <anup.sukumaran9@gmail.com>
2019-04-27 13:37:38 +0530

    Initial Commit

---
+++ PinViewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PinViewTest
+//
+//  Created by Sukumar Anup Sukumaran on 27/04/19.
+//  Copyright © 2019 TechTonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
